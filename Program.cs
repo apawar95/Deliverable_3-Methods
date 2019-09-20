1f9c6365d0081ea1895d609a2d08d484884100ec
Anushka Pawar <apawar@usf.edu>
2019-09-19 20:58:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Deliverable_3_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+
+            {
+                name(); 
+
+            }
+            void name()
+            {
+                Console.WriteLine("please provide your full name");
+                string username = Console.ReadLine();
+                Console.WriteLine("HELLO" + username + "!");
+
+            }
+        }
+    }
+}
